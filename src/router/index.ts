e5766e323d8e208d3a4a7c8970498798f0bca275
Cutomiya <2891641737@qq.com>
2023-05-08 11:24:49 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import {createRouter,createWebHashHistory} from "vue-router"
+
+const routes = [
+  {
+    path: '/',
+    name: 'layout',
+    component: () => import('../views/Login.vue')
+  }
+]
+//创建路由实例并传递routes配置
+const router = createRouter({
+  //内部提供了history模式的实现。为了简单起见，我们在这里使用hash模式。
+  history: createWebHashHistory(),
+  routes, //'routes: routes' 的缩写
+})
+
+export default router
