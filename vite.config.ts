e2157c8d5b22b972ec021508ea924bf7cab8af46
Cutomiya <2891641737@qq.com>
2023-05-11 17:55:58 +0800

    代理配置（不确定是否能正常运行）

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: { //路径配置
+  resolve: {
+    //路径配置
     alias: [
-        {
-            find: '@',
-            replacement: resolve(__dirname, './src'),
-        }
-    ]
-  }
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASEURL, // 后台接口
+        changeOrigin: true,
+        // secure: false, // 如果是https接口，需要配置这个参数
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
