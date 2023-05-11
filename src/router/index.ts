44cb5e627ae005c01d0294ee62f6038d0085d5f6
Cutomiya <2891641737@qq.com>
2023-05-11 09:38:39 +0800

    路由对象 RouteRecordRaw 类型断言

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
-import {createRouter,createWebHistory} from "vue-router"
+import {createRouter,createWebHistory,RouteRecordRaw} from "vue-router"
 
 const routes = [
   {
     path: '/login',
     name: 'login',
     component: () => import('@/views/Login.vue')
-  }, {
+  } as RouteRecordRaw, {
     path: '/layout',
     name: 'layout',
     component: () => import('@/views/Layout.vue'),
     // children: []
-  }, {
+  } as RouteRecordRaw, {
     path: '/',
     redirect: '/layout'
   }
 ]
 //创建路由实例并传递routes配置
 const router = createRouter({
-  //内部提供了history模式的实现。为了简单起见，我们在这里使用hash模式。
   history: createWebHistory(),
   routes, //'routes: routes' 的缩写
 })
